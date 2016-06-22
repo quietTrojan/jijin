e468d37bc99b9778beac470aa3267efa0b823c4f
zhangtianxiang <zhangtianxiang@ucfgroup.com>
2016-06-22 20:22:30 +0800

    快速提交

--- dist/js/test02.js
+++ dist/js/test02.js
@@ -5,8 +5,13 @@ $(function(){
         submitLink.get(0).click();
         //submitLink.trigger('click');
     });
-    $('#testLink').on('click',function(event){
-        $(this).attr('href','http://www.baidu.com');
+    $('.testLink').on('click',function(event){
+        var flag=$(this).data('flag');
+        if(flag){
+            $(this).attr('href','http://www.baidu.com');
+        }else{
+            event.preventDefault();
+        }
         //event.preventDefault();
     })
 });
