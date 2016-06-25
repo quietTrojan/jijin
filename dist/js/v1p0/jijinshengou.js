f54815eab712f4643d478a8e096c61d31e3eaa78
zhangtianxiang <zhangtianxiang@ucfgroup.com>
2016-06-25 11:30:33 +0800

    快速提交

--- dist/js/v1p0/jijinshengou.js
+++ dist/js/v1p0/jijinshengou.js
@@ -7,18 +7,4 @@ $(function(){
     $('#btn_know').on('click',function(){
         maskDiv.add(ratePopBox).hide();
     });
-    var numkeyBoard=new NumkeyBoard({
-        "inputBox":$('.keyBoardInput'),
-        "verifyFn":function(curNum,inputText){
-            if(inputText.indexOf('.')!=-1){
-                if(curNum=="."){
-                    return "";
-                }
-            }
-            if(/\.\d{2}/.test(inputText)){
-                return "";
-            }
-            return curNum;
-        }
-    });
 });
