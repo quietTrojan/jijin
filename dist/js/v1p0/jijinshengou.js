a2a3b71ba4fe3e0c02bea8a68dd22f9c74f0613c
zhangtianxiang <zhangtianxiang@ucfgroup.com>
2016-06-25 12:57:26 +0800

    快速提交

--- dist/js/v1p0/jijinshengou.js
+++ dist/js/v1p0/jijinshengou.js
@@ -7,4 +7,12 @@ $(function(){
     $('#btn_know').on('click',function(){
         maskDiv.add(ratePopBox).hide();
     });
+    $('#moneyInput').on('input',function(){
+        var inputVal=$(this).val();
+        var newVal='';
+        if(/^\d+\.\d{3,}$/.test(inputVal)){
+            newVal=inputVal.match(/^\d+\.\d{2}/)[0];
+            $(this).val(newVal);
+        }
+    });
 });
